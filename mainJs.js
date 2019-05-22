7f524b6ceea917fcb12ee2fbf17a75beefa31597
hayaMushka19Bday <50903847+hayaMushka19Bday@users.noreply.github.com>
2019-05-22 19:30:12 +0300

    moving balons

--- mainJs.js
+++ mainJs.js
@@ -1,3 +1,65 @@
+setTimeout(function(){
+    document.getElementById('downBaloon1').style.display ="none";
+    document.getElementById('downBaloon2').style.display ="none";
+    document.getElementById('downBaloon3').style.display ="none";
+    document.getElementById('downBaloon4').style.display ="none";
+    document.getElementById('downBaloon5').style.display ="none";
+    document.getElementById('upBaloon1').style.visibility ='visible';
+    document.getElementById('upBaloon2').style.visibility ="visible";
+    document.getElementById('upBaloon3').style.visibility ="visible";
+    document.getElementById('upBaloon4').style.visibility ="visible";
+    document.getElementById('upBaloon5').style.visibility ="visible";
+    setTimeout(function(){
+
+        document.getElementById('upBaloon2').style.animationDuration ="4.4s";
+
+        document.getElementById('upBaloon4').style.animationDuration ="4.4s";
+
+    },150);
+    setTimeout(function(){
+
+        document.getElementById('upBaloon2').style.animationDuration ="4.3s";
+
+        document.getElementById('upBaloon4').style.animationDuration ="4.3s";
+
+    },250);
+    setTimeout(function(){
+
+        document.getElementById('upBaloon2').style.animationDuration ="4.2s";
+
+        document.getElementById('upBaloon4').style.animationDuration ="4.2s";
+
+    },350);
+    setTimeout(function(){
+
+        document.getElementById('upBaloon2').style.animationDuration ="4.2s";
+
+        document.getElementById('upBaloon4').style.animationDuration ="4.2s";
+
+    },450);
+    setTimeout(function(){
+
+        document.getElementById('upBaloon2').style.animationDuration ="4.1s";
+
+        document.getElementById('upBaloon4').style.animationDuration ="4.1s";
+
+    },550);
+    setTimeout(function(){
+
+        document.getElementById('upBaloon2').style.animationDuration ="4s";
+
+        document.getElementById('upBaloon4').style.animationDuration ="4s";
+
+    },650);
+    setTimeout(function(){
+
+        document.getElementById('upBaloon2').style.animationDuration ="4s";
+
+        document.getElementById('upBaloon4').style.animationDuration ="4s";
+
+    },750);
+    
+   }, 7000);  
  function ani(){
     fadeOutEffect();
             //document.getElementById('plane').style.display = 'none';
@@ -14,8 +76,8 @@ const url='https://salty-lake-51238.herokuapp.com/mail';
 Http.open("GET", url);
 Http.send();
 Http.onreadystatechange=(e)=>{
-console.log(Http.responseText)
-}
+console.log(Http.responseText);
+};
 function fadeOutEffect() {
     var fadeTarget = document.getElementById("plane");
     var fadeEffect = setInterval(function () {
@@ -28,7 +90,14 @@ function fadeOutEffect() {
             clearInterval(fadeEffect);
         }
     }, 200);
-}
+};
+
+
+
+ 
+ }
+        function asad(){
+
         }
         function anitwo(){
             //document.getElementById('bg').className ='animation2';
