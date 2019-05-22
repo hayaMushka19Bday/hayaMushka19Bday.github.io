6f2dbb356ae8672adcaf7b28e8ef5eefcd3dfd6c
hayaMushka19Bday <50903847+hayaMushka19Bday@users.noreply.github.com>
2019-05-22 15:05:58 +0300

    Add files via upload

--- mainJs.js
+++ mainJs.js
@@ -1,5 +1,14 @@
  function ani(){
-            document.getElementById('plane').className ='animation';
+    fadeOutEffect();
+            //document.getElementById('plane').style.display = 'none';
+             document.getElementById('btn-boarder').style.border ='none' ;
+            document.getElementById('asdd').className = 'lds-eclipse';
+            setTimeout(function(){ document.getElementById('btn-boarder').style.border ="2px solid #fff" ;
+             document.getElementById('btn-boarder').style.borderRadius ="50%" ; 
+             document.getElementById('bg').className ='animation2';}, 1600);
+
+
+           
      const Http = new XMLHttpRequest();
 const url='https://salty-lake-51238.herokuapp.com/mail';
 Http.open("GET", url);
@@ -7,7 +16,20 @@ Http.send();
 Http.onreadystatechange=(e)=>{
 console.log(Http.responseText)
 }
+function fadeOutEffect() {
+    var fadeTarget = document.getElementById("plane");
+    var fadeEffect = setInterval(function () {
+        if (!fadeTarget.style.opacity) {
+            fadeTarget.style.opacity = 1;
+        }
+        if (fadeTarget.style.opacity > 0) {
+            fadeTarget.style.opacity -= 0.1;
+        } else {
+            clearInterval(fadeEffect);
+        }
+    }, 200);
+}
         }
         function anitwo(){
-            document.getElementById('bg').className ='animation2';
+            //document.getElementById('bg').className ='animation2';
         }
